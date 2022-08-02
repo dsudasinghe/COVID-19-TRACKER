bfa385b7ea1b938e449a5c230fcf3f477ce605ab
Dananjaya Sudasinghe <dananjayasudasinghe@gmail.com>
2022-08-02 23:58:04 +0530

    app completed

--- src/App.js
+++ src/App.js
@@ -9,10 +9,28 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import InfoBox from "./components/InfoBox";
 import Map from "./components/Map";
+import Table from "./components/Table";
+import { prettyPrintStat, sortData } from "./util";
+import LineGraph from "./components/LineGraph";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("Worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+  const [casesType, setCasesType] = useState("cases");
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
 
   //https://disease.sh/v3/covid-19/countries
   useEffect(() => {
@@ -24,6 +42,11 @@ function App() {
             name: country.country,
             value: country.countryInfo.iso2,
           }));
+
+          const sortedData = sortData(data);
+
+          setTableData(sortedData);
+          setMapCountries(data);
           setCountries(countries);
         });
     };
@@ -32,7 +55,6 @@ function App() {
 
   const onCountryChange = async (e) => {
     const countryCode = e.target.value;
-    setCountry(countryCode);
 
     const url =
       countryCode === "Worldwide"
@@ -41,6 +63,16 @@ function App() {
 
     //https://disease.sh/v3/covid-19/countries/all
     //https://disease.sh/v3/covid-19/countries/{country code}
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
+      });
   };
 
   return (
@@ -63,17 +95,44 @@ function App() {
         </div>
 
         <div className="app__stats">
-          <InfoBox title="Coronavirus Cases" cases={300} total={500}></InfoBox>
-          <InfoBox title="Recovered" cases={1000} total={4500}></InfoBox>
-          <InfoBox title="Deaths" cases={4600} total={2300}></InfoBox>
+          <InfoBox
+            isRed
+            active={casesType === "cases"}
+            onClick={(e) => setCasesType("cases")}
+            title="Coronavirus Cases"
+            cases={prettyPrintStat(countryInfo.todayCases)}
+            total={prettyPrintStat(countryInfo.cases)}
+          ></InfoBox>
+          <InfoBox
+            active={casesType === "recovered"}
+            onClick={(e) => setCasesType("recovered")}
+            title="Recovered"
+            cases={prettyPrintStat(countryInfo.todayRecovered)}
+            total={prettyPrintStat(countryInfo.recovered)}
+          ></InfoBox>
+          <InfoBox
+            isRed
+            active={casesType === "deaths"}
+            onClick={(e) => setCasesType("deaths")}
+            title="Deaths"
+            cases={prettyPrintStat(countryInfo.todayDeaths)}
+            total={prettyPrintStat(countryInfo.deaths)}
+          ></InfoBox>
         </div>
-        <Map></Map>
+        <Map
+          casesType={casesType}
+          countries={mapCountries}
+          center={mapCenter}
+          zoom={mapZoom}
+        />
       </div>
 
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by Country</h3>
-          <h3>Worldwide New Cases</h3>
+          <Table countries={tableData} />
+          <h3 className="app__graphTitle">Worldwide new {casesType}</h3>
+          <LineGraph className="app__graph" casesType={casesType} />
         </CardContent>
       </Card>
     </div>
---
+++ src/components/Table.jsx
@@ -0,0 +1,20 @@
+import numeral from "numeral";
+import React from "react";
+import "../styles/Table.css";
+
+function Table({ countries }) {
+  return (
+    <div className="table">
+      {countries.map(({ country, cases }) => (
+        <tr>
+          <td>{country}</td>
+          <td>
+            <strong>{numeral(cases).format("0,0")}</strong>
+          </td>
+        </tr>
+      ))}
+    </div>
+  );
+}
+
+export default Table;
